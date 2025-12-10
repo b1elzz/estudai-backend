a3bf8d23254c8d199e85069951f5c50dacf3532a
b1elzz <gabriel050monteiro@gmail.com>
2025-12-10 16:10:19 -0300

    fix: adicionar EnableAsync na aplicacao
    - Habilitar suporte a metodos assincronos
    - Necessario para envio de email nao bloqueante

--- src/main/java/br/com/fundatec/estudai/estudai/EstudaiApplication.java
+++ src/main/java/br/com/fundatec/estudai/estudai/EstudaiApplication.java
@@ -2,9 +2,11 @@ package br.com.fundatec.estudai.estudai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
 @EnableScheduling
 public class EstudaiApplication {
 
